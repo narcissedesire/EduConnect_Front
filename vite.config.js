c12d85d01dc66a3beb5017526b979686ce5d48ab
narcissedesire <116617142+narcissedesire@users.noreply.github.com>
2025-02-10 11:27:00 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    allowedHosts: ["educonnect-back.onrender.com", "localhost"],
     proxy: {
       "/api": {
         // target: "http://127.0.0.1:5000",
