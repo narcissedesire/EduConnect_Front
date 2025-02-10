c7e806a5710ad88ecca5e22bbed6d5a8cfeef52e
narcissedesire <116617142+narcissedesire@users.noreply.github.com>
2025-02-10 10:23:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000",
-        // target: "https://educonnect-back.onrender.com",
+        // target: "http://127.0.0.1:5000",
+        target: "https://educonnect-back.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
         headers: {
