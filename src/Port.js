ae886b6611e5eb6e8bc0b7540a60696f5abe0f4b
narcissedesire <116617142+narcissedesire@users.noreply.github.com>
2025-02-16 23:45:13 +0300

    Update Port.js

--- src/Port.js
+++ src/Port.js
@@ -1,3 +1,4 @@
-export const PortAPI = "http://127.0.0.1:5000/api";
+// export const PortAPI = "http://127.0.0.1:5000/api";
+export const PortAPI = "https://educonnect-back.onrender.com";
 // export const Port = "http://127.0.0.1:5000";
 export const Port = "https://educonnect-back.onrender.com";
